69947bf24586a8eea630e0a9246f6cb144af5e71
Steven Brady <bradys@my.lanecc.edu>
2022-01-27 09:34:39 -0800

    trying to fix azure publish

--- CS296N80sGameFansite/CS296N80sGameFansite/Program.cs
+++ CS296N80sGameFansite/CS296N80sGameFansite/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace CS296N80sGameFansite
 {
@@ -18,6 +19,11 @@ namespace CS296N80sGameFansite
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
