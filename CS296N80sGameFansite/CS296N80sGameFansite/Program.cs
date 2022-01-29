e69167e99acd8bef1a3b5832fd30a2213e1e7931
Steven Brady <bradys@my.lanecc.edu>
2022-01-29 05:51:54 -0800

    fixed Program.cs file. had code that was not used, but could not be null. changed code to what last stable build had. works now.

--- CS296N80sGameFansite/CS296N80sGameFansite/Program.cs
+++ CS296N80sGameFansite/CS296N80sGameFansite/Program.cs
@@ -19,11 +19,6 @@ namespace CS296N80sGameFansite
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
