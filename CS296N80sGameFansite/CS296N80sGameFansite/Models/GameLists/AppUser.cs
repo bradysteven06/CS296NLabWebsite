926ed45808f87a9edb5c868e54b6982116da7462
Steven Brady <bradys@my.lanecc.edu>
2022-01-29 19:22:19 -0800

    added AppUser class and identity NuGet

---
+++ CS296N80sGameFansite/CS296N80sGameFansite/Models/GameLists/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS296N80sGameFansite.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ CS296N80sGameFansite/CS296N80sGameFansite/Models/GameLists/SeedData/SeedPlayed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS296N80sGameFansite.Models.GameLists.SeedData
+{
+    public class SeedPlayed
+    {
+    }
+}
