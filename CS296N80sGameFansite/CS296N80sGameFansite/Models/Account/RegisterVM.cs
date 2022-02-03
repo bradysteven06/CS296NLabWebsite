54c2e4796132f19ac57cc6cb2a2ccadec8d2ec3a
Steven Brady <bradys@my.lanecc.edu>
2022-02-03 06:58:00 -0800

    added file structure for registration

---
+++ CS296N80sGameFansite/CS296N80sGameFansite/Models/Account/RegisterVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS296N80sGameFansite.Models.Account
+{
+    public class RegisterVM
+    {
+    }
+}
